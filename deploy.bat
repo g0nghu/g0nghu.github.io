448c8e955ed019c1179a373385659a215f8696f9
g0nghu <qvweixiang@outlook.com>
2026-06-16 23:58:13 +0800

    change more part

--- deploy.bat
+++ deploy.bat
@@ -2,6 +2,7 @@
 cd /d %~dp0
 
 echo === Hugo Deploy ===
+
 echo [1/3] Building...
 call hugo --minify
 if %errorlevel% neq 0 (
@@ -9,21 +10,34 @@ if %errorlevel% neq 0 (
     pause
     exit /b 1
 )
-echo       Build OK.
+echo       OK.
 
-echo [2/3] Pushing...
+echo [2/3] Push source to master...
 set /p MSG="Commit message (default: Deploy): "
 if "%MSG%"=="" set MSG=Deploy
 git add -A
-git commit -m "%MSG%" --allow-empty
+git commit -m "%MSG%" --allow-empty --quiet
 git -c http.sslVerify=false push origin master
 if %errorlevel% neq 0 (
-    echo Push failed!
+    echo Push source failed!
     pause
     exit /b 1
 )
-echo       Push OK.
+echo       OK.
 
-echo [3/3] Monitoring Actions...
-python deploy_monitor.py
+echo [3/3] Push site to gh-pages...
+cd public
+if not exist .git (
+    git init
+    git checkout -b gh-pages
+    git remote add origin https://github.com/g0nghu/g0nghu.github.io.git
+)
+git add -A
+git commit -m "%MSG%" --allow-empty --quiet
+git -c http.sslVerify=false push -f origin gh-pages
+cd ..
+echo       OK.
+
+echo.
+echo Done! https://g0nghu.github.io/
 pause
